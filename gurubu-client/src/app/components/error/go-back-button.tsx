10997bc389dc47e5194c94c64957095204210748
huseyingulcu <huseyingulcuu@gmail.com>
2024-02-01 20:43:18 +0300

    fix(gurubu-client): format files

--- gurubu-client/src/app/components/error/go-back-button.tsx
+++ gurubu-client/src/app/components/error/go-back-button.tsx
@@ -4,22 +4,16 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export const GoBackButton = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<button
-			className="not-found-page__navigation__go-back"
-			onClick={() => {
-				router.back();
-			}}>
-			<Image
-				priority
-				src="/left-arrow.svg"
-				alt="left-arrow"
-				width={24}
-				height={24}
-			/>
-			Go back
-		</button>
-	);
+  return (
+    <button
+      className="not-found-page__navigation__go-back"
+      onClick={() => {
+        router.back();
+      }}>
+      <Image priority src="/left-arrow.svg" alt="left-arrow" width={24} height={24} />
+      Go back
+    </button>
+  );
 };
