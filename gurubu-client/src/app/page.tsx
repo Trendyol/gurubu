1717a003d35d4f9967e24b77107c1c00f0175fd4
huseyingulcu <huseyingulcuu@gmail.com>
2024-02-04 23:42:40 +0300

    style(gurubu-client): add pricing component to page

--- gurubu-client/src/app/page.tsx
+++ gurubu-client/src/app/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/common/footer";
 import Navbar from "@/components/common/navbar";
 import HeroSection from "@/components/page/heroSection";
 import Testimonial from "@/components/page/testimonial";
+import { Pricing } from "@/components/page/pricing";
 import "@/styles/page/style.scss";
 
 export default function Home() {
