7589ebff3ff6b54b1895b9a2acfca27212137d1e
Altay Simsek <altaysimsek16@gmail.com>
2024-02-07 23:15:54 +0300

    GRB-63 feat: image change & style update

--- gurubu-client/src/app/components/page/features.tsx
+++ gurubu-client/src/app/components/page/features.tsx
@@ -1,6 +1,16 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
+import cn from 'classnames';
 
 const Features = () => {
+  const [activeTab, setActiveTab] = useState("score")
+  
+  const scoreTabStyle = cn('features-tabs__score', {active: activeTab === 'score'});
+  const planningTabStyle = cn('features-tabs__planning', {active: activeTab === 'planning'});
+  const imageSrc = activeTab === "score" ? "/score-grooming-mock.svg" : "/planning-poker-mock.svg";
+
+
   return (
     <section className="features-section">
       <div className="features-heading">
@@ -9,14 +19,14 @@ const Features = () => {
         <p className="features-heading__subtext">It is enough to create a room and share the link with your friends. We do not store any of your information!</p>
       </div>
       <div className="features-spotlight">
-        <Image src={'/screen-mockup.svg'} width={768} height={512} alt="An example of in app visuals" />
+        <Image src={imageSrc} width={768} height={512} alt="An example of in app visuals" />
       </div>
       <div className="features-tabs">
-        <div className="features-tabs__score">
+        <div className={scoreTabStyle} onClick={() => setActiveTab("score")}>
           <p className="title">Score grooming</p>
           <p className="description">If you want to score and sort tasks according to more than one metric, this option is for you!</p>
         </div>
-        <div className="features-tabs__planning">
+        <div className={planningTabStyle} onClick={() => setActiveTab("planning")}>
           <p className="title">Planning poker</p>
           <p className="description">If you only want to determine story points, you can choose this option that will allow you to score according to Fibonacci.</p>
         </div>
