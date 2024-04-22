6cea698b2299bfb2e91644404f5224c17bed3c99
Mehmet Şentürk <mehmet.senturk@akinon.com>
2024-04-22 19:03:07 +0300

    refactor(gurubu-client): og image logic changed

--- gurubu-client/src/app/layout.tsx
+++ gurubu-client/src/app/layout.tsx
@@ -1,25 +1,9 @@
-import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import classNames from "classnames";
 import "@/styles/globals.scss";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"], variable: '--font-space-grotesk' });
-
-export const metadata: Metadata = {
-  title: "Gurubu | Simple, fast and practical grooming",
-  description: "Simple, fast and practical grooming.",
-  openGraph: {
-    images: [
-      {
-        url: "/api/og",
-        width: 1200,
-        height: 630,
-        alt: "Simple, fast and practical grooming.",
-      },
-    ],
-  },
-};
 
 export default function RootLayout({
   children,
